f878cf45328e84f03b18afc92f5f723360071351
DaviMendesDev <davi.mendes.dev@gmail.com>
2021-04-05 14:47:47 -0400

    linter and formatter setup done, also npmignore added

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
 export type M = { [key: string]: any };
 
-export default function only (obj: M, keys: Array<string|String>): Object {
-    const result: M = new Object();
+export default function only(obj: M, keys: string[]): object {
+  const result: M = new Object();
 
-    for (const key of keys) {
-        result[key.toString()] = obj[key.toString()];
-    }
-    
-    return result;
+  for (const key of keys) {
+    result[key] = obj[key];
+  }
+
+  return result;
 }
 
-export function onlyToReq(req: Request, keys: Array<string|String>): Object {
-    const result: M = new Object();
+export function onlyToReq(req: Request, keys: string[]): object {
+  const result: M = new Object();
 
-    if (! req.body)
-        return result;
+  if (!req.body) return result;
 
-    const body: M = req.body as M;
+  const body: M = req.body as M;
 
-    for (const key of keys) {
-        result[key.toString()] = body[key.toString()];
-    }
-    
-    return result;
+  for (const key of keys) {
+    result[key] = body[key];
+  }
+
+  return result;
 }
