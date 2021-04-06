7819f555be20237bafac4bb4e47b28caddf52400
DaviMendesDev <davi.mendes.dev@gmail.com>
2021-04-06 12:01:55 -0400

    string features working

--- src/__tests__/only.test.ts
+++ src/__tests__/only.test.ts
@@ -1,80 +1,112 @@
 import only, { M } from '../index';
 
-const personObject: object = {
-    name: "Phillipi",
-    age: 18,
-    birthday: new Date('06/06/2001')
+interface Person {
+  name: string;
+  age: number;
+  birthday: Date;
+}
+
+const defaultValues: Person = {
+  name: 'Phillipi',
+  age: 18,
+  birthday: new Date('06/06/2001'),
 };
+
+const personObject: object = defaultValues;
 
 const personArray: M = [];
 
-personArray[("name" as string)] = "Phillipi";
-personArray[("age" as string)] = 18,
-personArray[("birthday" as string)] = new Date('06/06/2001');
+personArray['name'] = defaultValues.name;
+personArray['age'] = defaultValues.age;
+personArray['birthday'] = defaultValues.birthday;
+
+// console.log(personObject, personArray);
 
 const emptyObject = {};
 const emptyArray: M = [];
 
 test(`should return only the 'age' from 'personObject' object`, () => {
-    const anAgeOnlyObject = { age: 18 };
-    const theCallOnly = only(personObject, [ 'age' ]);
+  const anAgeOnlyObject = { age: defaultValues.age };
+  const theCallOnly = only(personObject, ['age']);
 
-    // console.log(anAgeOnlyObject);
-    // console.log(theCallOnly);
+  // console.log(anAgeOnlyObject);
+  // console.log(theCallOnly);
 
-    expect(theCallOnly).toEqual(anAgeOnlyObject);
-})
+  expect(theCallOnly).toEqual(anAgeOnlyObject);
+});
 
 test(`should return only the 'age' from 'personArray' array`, () => {
-    const anAgeOnlyObject = { age: 18 };
-    const theCallOnly = only(personArray, [ 'age' ]);
+  const anAgeOnlyObject = { age: defaultValues.age };
+  const theCallOnly = only(personArray, ['age']);
 
-    // console.log(anAgeOnlyObject);
-    // console.log(theCallOnly);
+  // console.log(anAgeOnlyObject);
+  // console.log(theCallOnly);
 
-    expect(theCallOnly).toEqual(anAgeOnlyObject);
-})
+  expect(theCallOnly).toEqual(anAgeOnlyObject);
+});
 
 test(`should return empty object ({ }) from empty array ([ ])`, () => {
-    const anEmptyObject = { };
-    const theCallOnly = only(emptyArray, [ 'age' ]);
+  const anEmptyObject = {};
+  const theCallOnly = only(emptyArray, ['age']);
 
-    // console.log(anEmptyObject);
-    // console.log(theCallOnly);
+  // console.log(anEmptyObject);
+  // console.log(theCallOnly);
 
-    expect(theCallOnly).toEqual(anEmptyObject);
-})
+  expect(theCallOnly).toEqual(anEmptyObject);
+});
 
 test(`should return empty object ({ }) from empty object ({ })`, () => {
-    const anEmptyObject = { };
-    const theCallOnly = only(emptyObject, [ 'age' ]);
+  const anEmptyObject = {};
+  const theCallOnly = only(emptyObject, ['age']);
 
-    // console.log(anEmptyObject);
-    // console.log(theCallOnly);
+  // console.log(anEmptyObject);
+  // console.log(theCallOnly);
 
-    expect(theCallOnly).toEqual(anEmptyObject);
-})
+  expect(theCallOnly).toEqual(anEmptyObject);
+});
 
 test(`should return empty object ({ }) from object that doesn't have the given keys`, () => {
-    const wrongObject = only(personObject, ['name']);
+  const wrongObject = only(personObject, ['name']);
 
-    const anEmptyObject = { };
-    const theCallOnly = only(wrongObject, [ 'age' ]);
+  const anEmptyObject = {};
+  const theCallOnly = only(wrongObject, ['age']);
 
-    // console.log(anEmptyObject);
-    // console.log(theCallOnly);
+  // console.log(anEmptyObject);
+  // console.log(theCallOnly);
 
-    expect(theCallOnly).toEqual(anEmptyObject);
-})
+  expect(theCallOnly).toEqual(anEmptyObject);
+});
 
 test(`should return empty object ({ }) from array that doesn't have the given keys`, () => {
-    const wrongObject = only(personArray, ['name']);
+  const wrongObject = only(personArray, ['name']);
 
-    const anEmptyObject = { };
-    const theCallOnly = only(wrongObject, [ 'age' ]);
+  const anEmptyObject = {};
+  const theCallOnly = only(wrongObject, ['age']);
 
-    // console.log(anEmptyObject);
-    // console.log(theCallOnly);
+  // console.log(anEmptyObject);
+  // console.log(theCallOnly);
 
-    expect(theCallOnly).toEqual(anEmptyObject);
-})
+  expect(theCallOnly).toEqual(anEmptyObject);
+});
+
+test(`should return only the 'age' and 'birthday' from string key: ('age birthday')`, () => {
+  const personObjectWithouName = { age: defaultValues.age, birthday: defaultValues.birthday };
+  const theCallOnly = only(personObject, 'age birthday');
+
+  // console.log(personObjectWithouName);
+  // console.log(theCallOnly);
+
+  expect(theCallOnly).toEqual(personObjectWithouName);
+});
+
+test(`should return only the 'age' from string key: ('age birthday')`, () => {
+  const personWithoutBirthday = { name: defaultValues.name, age: defaultValues.age };
+
+  const anAgeOnlyObject = { age: defaultValues.age };
+  const theCallOnly = only(personWithoutBirthday, 'age birthday');
+
+  // console.log(anAgeOnlyObject);
+  // console.log(theCallOnly);
+
+  expect(theCallOnly).toEqual(anAgeOnlyObject);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 export type M = { [key: string]: any };
 
-export default function only(obj: M, keys: string[]): object {
+export default function only(obj: M, keys: string[] | string): object {
   const result: M = new Object();
 
+  if (typeof keys === 'string') keys = keys.split(' ');
+
   for (const key of keys) {
-    if (obj[key])
-      result[key] = obj[key];
+    if (obj[key]) result[key] = obj[key];
   }
 
   return result;
@@ -19,7 +20,7 @@ export function onlyToReq(req: Request, keys: string[]): object {
   const body: M = req.body as M;
 
   for (const key of keys) {
-    result[key] = body[key];
+    if (body[key]) result[key] = body[key];
   }
 
   return result;
