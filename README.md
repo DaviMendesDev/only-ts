ea812ae98febdc2cfbca65749575e2437810d5d1
DaviMendesDev <davi.mendes.dev@gmail.com>
2021-04-07 16:09:28 -0400

    aliases working fine, nested objects still on beta

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # only-ts
-A typescript library based on [only](https://github.com/tj/node-only) project
+[OnlyTS](https://github.com/DaviMendesDev/only-ts) is a typescript package based on [only](https://github.com/tj/node-only) project. It will receive an array or object and return only the expected values, some more examples are below.
+
+## Installation
+run the following command on your npm project.
+
+    $ npm i only-ts
+
+## Getting Started
+Let's imagine that we have a form-body-data-like this (like `req.body`) :
+
+```ts
+type FormData = {
+    [key: string]: any,
+}
+```
+and we receive this form data to create a new user:
+```ts
+const personData: FormData = {
+    name: 'Willson',
+    age: '24',
+    jwtoken: 'some-token',
+}
+```
+But we don't want that a user can sets up your own JWToken, so we just use only to extract what we want from this data, like that:
+
+```ts
+    import only from 'only-ts'
+
+    // ...
+
+    only(personData, ['name', 'age']); // = { name: 'Willson', age: '24' }
+
+    // ...
+```
+
+You can also use aliases:
+```ts
+    only(personData, ['name as username', 'age']); // = { username: 'Willson', age: '24' }
+```
+
+<!-- And you can get data from nested objects too:
+```ts
+    only({ user: personData }, ['user.name as username', 'age']); // = { username: 'Willson', age: '24' }
+```
+**Notice**: nested properties needs an alias to work. -->
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,48 @@
 export type M = { [key: string]: any };
 
+function getValue (obj: M, key: string): any {
+  if (key.indexOf('.') === -1)
+    return obj[key];
+  
+  const nextNestedObject: string = key.split('.')[0];
+  key = key.replace(`${nextNestedObject}.`, '');
+
+  return getValue(obj[nextNestedObject], key);
+}
+
+function getAlias (key: string): string {
+  if (key.indexOf(' as ') === -1)
+    return key;
+
+  return key.split(' as ').pop()!.trim();
+}
+
+function getOriginalKey (key: string): string {
+  if (key.indexOf(' as ') === -1)
+    return key;
+
+  return key.split(' as ')[0]!.trim();
+}
+
 export default function only(obj: M, keys: string[] | string): object {
   const result: M = new Object();
 
   if (typeof keys === 'string') keys = keys.split(' ');
 
-  for (const key of keys) {
-    if (obj[key]) result[key] = obj[key];
+  for (let key of keys) {
+    const alias: string = getAlias(key);
+    key = getOriginalKey(key);
+    const value = getValue(obj, key);
+    if (value) result[alias] = value;
   }
 
   return result;
 }
 
-export function onlyToReq(req: Request, keys: string[]): object {
-  const result: M = new Object();
+export function onlyToReq(req: Request, keys: string[] | string): object {
+  if (!req.body) return {};
 
-  if (!req.body) return result;
+  const body: M = req.body  as  M;
 
-  const body: M = req.body as M;
-
-  for (const key of keys) {
-    if (body[key]) result[key] = body[key];
-  }
-
-  return result;
+  return only(body, keys);
 }
